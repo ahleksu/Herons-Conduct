62ff12682ab3f0370933e9242ca99690bedee1cb
Negative-Newspaper <howard.bayquen@gmail.com>
2022-12-16 22:35:33 +0800

    incident referral ui

---
+++ app/src/main/java/com/umak/heronsconduct/CreateIncidentReferal.java
@@ -0,0 +1,14 @@
+package com.umak.heronsconduct;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateIncidentReferal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_incident_referal);
+    }
+}
