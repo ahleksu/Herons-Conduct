62315e225811b890e58cf287233a9c550f2da4c8
Negative-Newspaper <howard.bayquen@gmail.com>
2022-12-30 19:40:06 +0800

    search and view violations(not finish)

---
+++ app/src/main/java/com/umak/heronsconduct/searchAndViewViolation.java
@@ -0,0 +1,14 @@
+package com.umak.heronsconduct;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class searchAndViewViolation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_and_view_violation);
+    }
+}
