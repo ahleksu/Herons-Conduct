8c9901bd671a85a0371731827ffab2391935291f
customer <simonlegaspi27@gmail.com>
2023-01-05 16:26:21 +0800

    Register validation

--- app/src/main/java/com/umak/heronsconduct/Register_Parent.java
+++ app/src/main/java/com/umak/heronsconduct/Register_Parent.java
@@ -1,14 +1,182 @@
 package com.umak.heronsconduct;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class Register_Parent extends AppCompatActivity {
+
+
+
+    ImageButton cancelButton1, cancelButtonError1;
+    Button ok_btn1, ok_btnError1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_parent);
+
+
+        reg_stu1();
+
+    }
+
+
+    public void reg_stu1() {
+
+        EditText edtFNamePAR = findViewById(R.id.fNamePAR);
+        EditText edtMNamePAR = findViewById(R.id.mNamePAR);
+        EditText edtLNamePAR = findViewById(R.id.lNamePAR);
+        EditText edtAddressPAR = findViewById(R.id.addressParent);
+        EditText edtContactPAR = findViewById(R.id.contactParent);
+        EditText edtStudentIdPAR = findViewById(R.id.studentId_Parent);
+        EditText edtParent_ID = findViewById(R.id.parent_ID);
+        EditText edtEmailPAR = findViewById(R.id.parent_Email);
+        EditText edtPasswordPAR = findViewById(R.id.parent_Password);
+        EditText edtConfirmPasswordPAR = findViewById(R.id.parent_ConfirmPassword);
+
+        String FNamePAR = edtFNamePAR.getText().toString();
+        String MNamePAR = edtMNamePAR.getText().toString();
+        String LNamePAR = edtLNamePAR.getText().toString();
+        String AddressPAR = edtAddressPAR.getText().toString();
+        String ContactPAR = edtContactPAR.getText().toString();
+        String StudentIdPAR = edtStudentIdPAR.getText().toString();
+        String ParentIdPAR = edtParent_ID.getText().toString();
+        String EmailPAR = edtEmailPAR.getText().toString();
+        String PasswordPAR = edtPasswordPAR.getText().toString();
+        String ConfirmPasswordPAR = edtConfirmPasswordPAR.getText().toString();
+
+
+
+
+        Button Register_PAR_btn = findViewById(R.id.Register_PAR_btn);
+        Register_PAR_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String FNamePAR = edtFNamePAR.getText().toString();
+                String MNamePAR = edtMNamePAR.getText().toString();
+                String LNamePAR = edtLNamePAR.getText().toString();
+                String AddressPAR = edtAddressPAR.getText().toString();
+                String ContactPAR = edtContactPAR.getText().toString();
+                String StudentIdPAR = edtStudentIdPAR.getText().toString();
+                String ParentIdPAR = edtParent_ID.getText().toString();
+                String EmailPAR = edtEmailPAR.getText().toString();
+                String PasswordPAR = edtPasswordPAR.getText().toString();
+                String ConfirmPasswordPAR = edtConfirmPasswordPAR.getText().toString();
+
+                if(TextUtils.isEmpty(FNamePAR)|| TextUtils.isEmpty(MNamePAR) || TextUtils.isEmpty(LNamePAR) ||TextUtils.isEmpty(AddressPAR) ||TextUtils.isEmpty(ContactPAR) ||TextUtils.isEmpty(StudentIdPAR) ||TextUtils.isEmpty(ParentIdPAR) ||TextUtils.isEmpty(EmailPAR) ||TextUtils.isEmpty(PasswordPAR)) {
+                    openDialogError();
+                    return;
+                }
+                if(FNamePAR.isEmpty()) {
+                    openDialogError();
+                    return;
+                }
+
+                if(!ConfirmPasswordPAR.equals(PasswordPAR)) {
+                    Toast.makeText(Register_Parent.this, "Mismatch Password", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    openDialog();
+                }
+
+
+            }
+        });
+
+
+    }
+
+    private void openDialog() {
+        View alertCustomDialog = LayoutInflater.from(Register_Parent.this).inflate(R.layout.custom_dialog, null);
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(Register_Parent.this);
+
+        alertDialog.setView(alertCustomDialog);
+        cancelButton1 = (ImageButton) alertCustomDialog.findViewById(R.id.cancelID);
+        ok_btn1 = (Button) alertCustomDialog.findViewById(R.id.ok_btn_id);
+
+        final AlertDialog dialog = alertDialog.create();
+
+
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.show();
+
+
+        cancelButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.cancel();
+
+            }
+        });
+        ok_btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.cancel();
+                Intent intent = new Intent (getApplicationContext(), Register.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+
+    private void openDialogError() {
+        View alertCustomDialog = LayoutInflater.from(Register_Parent.this).inflate(R.layout.custom_dialog_error, null);
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(Register_Parent.this);
+
+        alertDialog.setView(alertCustomDialog);
+        cancelButtonError1 = (ImageButton) alertCustomDialog.findViewById(R.id.cancelID_error);
+        ok_btnError1 = (Button) alertCustomDialog.findViewById(R.id.ok_btn_id_error);
+
+        final AlertDialog dialog = alertDialog.create();
+
+
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.show();
+
+
+        cancelButtonError1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.cancel();
+
+            }
+        });
+        ok_btnError1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.cancel();
+            }
+        });
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public void x_back1 (View view) {
+        Intent intent = new Intent(getApplicationContext(), Register.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/umak/heronsconduct/Register_Student.java
+++ app/src/main/java/com/umak/heronsconduct/Register_Student.java
@@ -1,14 +1,187 @@
 package com.umak.heronsconduct;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class Register_Student extends AppCompatActivity {
+
+
+
+
+    ImageButton cancelButton, cancelButtonError;
+    Button ok_btn, ok_btnError;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_student);
+
+        reg_stu();
+
+
+        Button have_acc = findViewById(R.id.login_haveAccSTU);
+
+        have_acc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Register.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
+
+    public void reg_stu() {
+        EditText edtFNameSTU = findViewById(R.id.fNameStudent);
+        EditText edtMNameSTU = findViewById(R.id.mNameStudent);
+        EditText edtLNameSTU = findViewById(R.id.lNameStudent);
+        EditText edtGenderSTU = findViewById(R.id.genderStudent);
+        EditText edtBirthSTU = findViewById(R.id.birthdateStudent);
+        EditText edtAddressSTU = findViewById(R.id.addressStudent);
+        EditText edtContactNumSTU = findViewById(R.id.contactNumberStudent);
+        EditText edtUmakEmailSTU = findViewById(R.id.umakEmailStudent);
+        EditText edtStudentID = findViewById(R.id.studentID);
+        EditText edtPersonalEmailSTU = findViewById(R.id.personalEmail_Student);
+        EditText edtCollegeSTU = findViewById(R.id.collegeStudent);
+        EditText edtYearSTU = findViewById(R.id.yearLevelStudent);
+        EditText edtCourseSTU = findViewById(R.id.courseStudent);
+        EditText edtPasswordSTU = findViewById(R.id.passwordStudent);
+        EditText edtConfirmPasswordSTU = findViewById(R.id.ConfirmPassword_Student);
+
+
+
+
+
+        Button reg_stu = findViewById(R.id.Register_STU);
+        reg_stu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String FNameSTU = edtFNameSTU.getText().toString();
+                String MNameSTU = edtMNameSTU.getText().toString();
+                String LNameSTU = edtLNameSTU.getText().toString();
+                String GenderSTU = edtGenderSTU.getText().toString();
+                String BirthSTU = edtBirthSTU.getText().toString();
+                String AddressSTU = edtAddressSTU.getText().toString();
+                String ContactNumSTU = edtContactNumSTU.getText().toString();
+                String UmakEmailSTU = edtUmakEmailSTU.getText().toString();
+                String StudentIDSTU = edtStudentID.getText().toString();
+                String PersonalEmailSTU = edtPersonalEmailSTU.getText().toString();
+                String CollegeSTU = edtCollegeSTU.getText().toString();
+                String YearSTU = edtYearSTU.getText().toString();
+                String CourseSTU = edtCourseSTU.getText().toString();
+                String PasswordSTU = edtPasswordSTU.getText().toString();
+                String ConfirmPasswordSTU = edtConfirmPasswordSTU.getText().toString();
+
+
+                if(TextUtils.isEmpty(FNameSTU) || TextUtils.isEmpty(MNameSTU) || TextUtils.isEmpty(LNameSTU) || TextUtils.isEmpty(GenderSTU) || TextUtils.isEmpty(BirthSTU) || TextUtils.isEmpty(AddressSTU) || TextUtils.isEmpty(ContactNumSTU) || TextUtils.isEmpty(UmakEmailSTU) || TextUtils.isEmpty(StudentIDSTU) ||
+                        TextUtils.isEmpty(PersonalEmailSTU) || TextUtils.isEmpty(CollegeSTU) || TextUtils.isEmpty(YearSTU) || TextUtils.isEmpty(CourseSTU) || TextUtils.isEmpty(PasswordSTU)) {
+                    openDialogError();
+                    return;
+                }
+
+                if(FNameSTU.isEmpty()) {
+                    openDialogError();
+                    return;
+                }
+
+                if(!ConfirmPasswordSTU.equals(PasswordSTU)) {
+                    Toast.makeText(Register_Student.this, "Mismatch Password", Toast.LENGTH_SHORT).show();
+                }
+
+                else {
+                    openDialog();
+                }
+
+            }
+        });
+    }
+
+    private void openDialog() {
+        View alertCustomDialog = LayoutInflater.from(Register_Student.this).inflate(R.layout.custom_dialog, null);
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(Register_Student.this);
+
+        alertDialog.setView(alertCustomDialog);
+        cancelButton = (ImageButton) alertCustomDialog.findViewById(R.id.cancelID);
+        ok_btn = (Button) alertCustomDialog.findViewById(R.id.ok_btn_id);
+
+        final AlertDialog dialog = alertDialog.create();
+
+
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.show();
+
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.cancel();
+
+            }
+        });
+        ok_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.cancel();
+                Intent intent = new Intent (getApplicationContext(), Register.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+
+    private void openDialogError() {
+        View alertCustomDialog = LayoutInflater.from(Register_Student.this).inflate(R.layout.custom_dialog_error, null);
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(Register_Student.this);
+
+        alertDialog.setView(alertCustomDialog);
+        cancelButtonError = (ImageButton) alertCustomDialog.findViewById(R.id.cancelID_error);
+        ok_btnError = (Button) alertCustomDialog.findViewById(R.id.ok_btn_id_error);
+
+        final AlertDialog dialog = alertDialog.create();
+
+
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.show();
+
+
+        cancelButtonError.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.cancel();
+
+            }
+        });
+        ok_btnError.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.cancel();
+            }
+        });
+    }
+
+
+    
+
+
+    public void x_back(View view) {
+
+        Intent intent = new Intent(getApplicationContext(), Register.class);
+        startActivity(intent);
+
+    }
+
 }
