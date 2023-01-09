8d8801c35b0876712850dd70bab9d4aa8485f1a6
ahleksu <roblesjohnalex18@gmail.com>
2023-01-09 22:22:54 +0800

    Added Accountabilities Page

---
+++ app/src/main/java/com/umak/heronsconduct/student_parent/Accountabilities.java
@@ -0,0 +1,16 @@
+package com.umak.heronsconduct.student_parent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.umak.heronsconduct.R;
+
+public class Accountabilities extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accountabilities);
+    }
+}
