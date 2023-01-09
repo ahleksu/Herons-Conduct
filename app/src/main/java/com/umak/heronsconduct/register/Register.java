e06dd5605791857c8a5d47b3c7dd0655e6be52f5
ahleksu <roblesjohnalex18@gmail.com>
2023-01-09 20:16:20 +0800

    Revised Select User Type

--- app/src/main/java/com/umak/heronsconduct/register/Register.java
+++ app/src/main/java/com/umak/heronsconduct/register/Register.java
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.umak.heronsconduct.R;
+import com.umak.heronsconduct.login.Login;
 
 public class Register extends AppCompatActivity {
 
-
+    Button signIn;
 
     static String Account = "";
 
@@ -19,6 +21,16 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        signIn = findViewById(R.id.login_haveAcc_parent);
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
 
     }
