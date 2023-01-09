e9b4bb7443271652c8c1cf6ef7e20608bc0b1a5a
Negative-Newspaper <howard.bayquen@gmail.com>
2023-01-09 22:22:21 +0800

    button

--- app/src/main/java/com/umak/heronsconduct/general/AboutHeronsConduct.java
+++ app/src/main/java/com/umak/heronsconduct/general/AboutHeronsConduct.java
@@ -3,14 +3,17 @@ package com.umak.heronsconduct.general;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import com.umak.heronsconduct.R;
 
 public class AboutHeronsConduct extends AppCompatActivity {
-
+    ImageButton backBtnAboutHeronsConduct;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_herons_conduct);
+
+
     }
 }
